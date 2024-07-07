24322998075f66e33b49540b33a00635876c08c8
Kacper Kafara <kacperkafara@gmail.com>
2024-07-07 12:02:50 +0200

    Remove unused Map component

--- packages/front/src/App.tsx
+++ packages/front/src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import map from './assets/maps/m0-overworld.png';
 import './App.css';
 
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from './server';
-import Map from './components/Map';
 
 import categories from './data/categories.json';
 import data from './data/items.json';
